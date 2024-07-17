cb7d2e1981bfc69dfd2971a5afebb32cfcc8dec1
Bright Atsighi <brytebee@gmail.com>
2024-07-17 11:15:27 +0100

    Created a counter screen with usereducer

---
+++ src/screens/CounterScreenRed.js
@@ -0,0 +1,48 @@
+import React, { useReducer, useState } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+
+const UPDATE_VALUE = 10;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increase_count":
+      return { ...state, counter: state.counter + action.payload };
+    case "reduce_count":
+      return state.counter - action.payload < 0
+        ? state
+        : { ...state, counter: state.counter - action.payload };
+    default:
+      return state;
+  }
+};
+
+const CounterScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const { counter } = state;
+
+  return (
+    <View>
+      <Button
+        title="Increase"
+        onPress={() =>
+          dispatch({ type: "increase_count", payload: UPDATE_VALUE })
+        }
+      />
+      <Button
+        title="Decrease"
+        onPress={() =>
+          dispatch({ type: "reduce_count", payload: UPDATE_VALUE })
+        }
+      />
+      <Text style={styles.textStyle}>Counter: {counter}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    textAlign: "center",
+  },
+});
+
+export default CounterScreen;
